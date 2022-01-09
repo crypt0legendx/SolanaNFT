6e7538b852cfea0d462e01dba4c655615d23d0b4
real-fullstack-developer <95104528+real-fullstack-developer@users.noreply.github.com>
2022-01-09 00:05:12 -0800

    create wallet

--- index.js
+++ index.js
@@ -7,4 +7,35 @@ var splToken = require('@solana/spl-token');
         web3.clusterApiUrl("devnet"),
         'confirmed',
     );
+
+    // Generate a new wallet keypair and airdrop SOL
+    var fromWallet = web3.Keypair.generate();
+    var fromAirdropSignature = await connection.requestAirdrop(
+        fromWallet.publicKey,
+        web3.LAMPORTS_PER_SOL,
+    );
+    //wait for airdrop confirmation
+    await connection.confirmTransaction(fromAirdropSignature);
+
+    //create new token mint
+    let mint = await splToken.Token.createMint(
+        connection,
+        fromWallet,
+        fromWallet.publicKey,
+        null,
+        9,
+        splToken.TOKEN_PROGRAM_ID,
+    );
+
+    // get the token account of the fromwallet Solana address, if it does not exist, create it.
+    let fromTokenAccount = await mint.getOrCreateAssociatedAccountInfo(fromWallet.publicKey, );
+
+    var toWallet = web3.Keypair.generate();
+
+    //get the token account of the toWallet Solana address, if it is does not exist, create it.
+    var toTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+        toWallet.publicKey,
+    );
+
+
 })
